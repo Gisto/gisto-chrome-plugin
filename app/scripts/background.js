04854b81c91f6a4c8c74ede54117bbd10a4dc6c6
Maayan Glikser <maayan@glikm.com>
2013-09-01 00:17:30 +0300

    Added chrome notifications when gists arrived

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,3 +5,40 @@ chrome.runtime.onInstalled.addListener(function (details) {
 });
 
 var socket = io.connect('http://localhost:3000/');
+var notificationCounter = 0;
+
+socket.on('identify', function() {
+    var user = localStorage.getItem('githubUser');
+    console.log(user);
+    if (user) {
+        console.log('emitting connect event');
+        socket.emit('registerClient', { user: user, token: 'hjdIO783HKL782HK@&*73H9&@!(jd', plugin: true });
+
+        var opt = {
+            type: "basic",
+            title: "Connected to notification server",
+            message: "Connection was successful",
+            iconUrl: "images/icon.png"
+        };
+
+        chrome.notifications.create('gisto-connected', opt, function() {});
+    }
+});
+
+socket.on('receiveNotification', function(data) {
+
+    // data - { sender: client.user, gistId: data.gistId, name: data.name, gravatar_id: data.gravatar_id})
+    var options = {
+        type: "basic",
+        title: "You recieved a Gist on Gisto",
+        message: "You were sent " + data.name + " by " + data.sender,
+        iconUrl: "https://secure.gravatar.com/avatar/" + data.gravatar_id
+    };
+
+    chrome.notifications.create('gisto_gist_received', options, function() {});
+
+    chrome.browserAction.setBadgeText({
+        text: ++notificationCounter + ""
+    });
+
+});
